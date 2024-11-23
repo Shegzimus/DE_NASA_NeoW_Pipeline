b09fc6a5082e845a9f74dabbb21c8b741b6ef80a
Shegzimus <segun.ajetunmobi@gmail.com>
2024-11-23 22:33:14 +0100

    Removed unnecessary paths

--- airflow/utils/constants.py
+++ airflow/utils/constants.py
@@ -1,21 +1,14 @@
 import configparser
-import sys
-import os
-
-# sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Create a configparser object
 config = configparser.ConfigParser()
 
 # Read the .conf file
-config.read('airflow\config\secrets.conf')
+config.read('airflow/config/secrets.conf')
 
 # Access the secrets from the file
 nasa_api_key = config['API']['nasa_api_key']
-gcs_bucket = config['GCS']['gcs_bucket']
-gcs_project_id = config['GCS']['gcs_project_id']
-bigquery_project_id = config['BigQuery']['bigquery_project_id']
-bigquery_dataset = config['BigQuery']['bigquery_dataset']
+
 
 # URLs
 # neo_feed =f'https://api.nasa.gov/neo/rest/v1/feed?start_date={START_DATE}&end_date={END_DATE}&api_key={API_KEY}'
