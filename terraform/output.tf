4adb0083e51053ba785e45de47fd79acd5574eb1
Shegzimus <segun.ajetunmobi@gmail.com>
2024-12-06 08:30:14 +0100

    Added bucket name

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,7 @@ output "BQ_DATASET_STAGING" {
 output "BQ_DATASET_WAREHOUSE" {
   value = var.bq_dataset_warehouse
 }
+
+output "data_lake_bucket_name" {
+  value = google_storage_bucket.data-lake-bucket.name
+}
