19ee0a37bcdb348ab508c9ab559bf605646e50f1
Shegzimus <segun.ajetunmobi@gmail.com>
2025-01-03 00:38:01 +0100

    reverted to .conf usage; used the configparser

--- airflow/utils/constants.py
+++ airflow/utils/constants.py
@@ -1,27 +1,17 @@
 import configparser
+import os
 
 # Create a configparser object
-config = configparser.ConfigParser()
+secrets = configparser.ConfigParser()
 
 # Read the .conf file
-config.read('airflow/config/secrets.conf')
+secrets.read('/opt/airflow/config/secrets.conf')
+# config.read(os.path.join(os.path.dirname(__file__), '../config/secrets.conf'))
 
-# Access the secrets from the file
-nasa_api_key = config['API']['nasa_api_key']
-
-
-# URLs
-# neo_feed =f'https://api.nasa.gov/neo/rest/v1/feed?start_date={START_DATE}&end_date={END_DATE}&api_key={API_KEY}'
-# neo_lookup = f'https://api.nasa.gov/neo/rest/v1/neo/{ASTEROID_ID}?api_key={API_KEY}'
-# neo_browse = f'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key={API_KEY}'
-
-# Paths for testing
-LOCAL_OUTPUT_PATH = 'airflow/data/output'
-
-LOCAL_INPUT_PATH = 'airflow/data/input'
-
-# Paths for production 
-
-OUTPUT_PATH = config['file_paths']['output_path']
-
-INPUT_PATH = config['file_paths']['input_path']
+nasa_api_key = secrets.get('API', 'nasa_api_key')
+# # Access the secrets from the file
+# try:
+#     with open('/run/secrets/api_key_config', 'r') as secret_file:
+#         nasa_api_key = secret_file.read().strip()
+# except FileNotFoundError:
+#     raise RuntimeError("The API key secret file was not found.")
