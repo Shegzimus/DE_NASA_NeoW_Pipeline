d8759d8084b4c9125e56d35b7e17a3fdf4bea867
Shegzimus <segun.ajetunmobi@gmail.com>
2025-01-04 17:23:56 +0100

    cleaned up comment code

--- airflow/utils/constants.py
+++ airflow/utils/constants.py
@@ -6,12 +6,6 @@ secrets = configparser.ConfigParser()
 
 # Read the .conf file
 secrets.read('/opt/airflow/config/secrets.conf')
-# config.read(os.path.join(os.path.dirname(__file__), '../config/secrets.conf'))
+
 
 nasa_api_key = secrets.get('API', 'nasa_api_key')
-# # Access the secrets from the file
-# try:
-#     with open('/run/secrets/api_key_config', 'r') as secret_file:
-#         nasa_api_key = secret_file.read().strip()
-# except FileNotFoundError:
-#     raise RuntimeError("The API key secret file was not found.")
