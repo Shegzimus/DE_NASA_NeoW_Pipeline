7aefe82059c74f83647512736ed07791c403ff09
Shegzimus <segun.ajetunmobi@gmail.com>
2025-01-08 14:35:27 +0100

    minor changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -37,3 +37,15 @@ variable "bq_dataset_warehouse" {
   type = string
   default = "de_dataset_warehouse"
 }
+
+output "BQ_DATASET_STAGING" {
+  value = var.bq_dataset_staging
+}
+
+output "BQ_DATASET_WAREHOUSE" {
+  value = var.bq_dataset_warehouse
+}
+
+output "data_lake_bucket_name" {
+  value = google_storage_bucket.data-lake-bucket.name
+}
